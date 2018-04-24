be4009ecafdced94facc23843f1e35cfa011bd8a
徐林昊 <linhao.xu@hypers.com>
2018-04-24 14:41:05 +0800

    feat: use HTTP for Hystrix, Turbine and Zikpin

--- composite/movie-api/src/main/java/com/hypers/api/Application.java
+++ composite/movie-api/src/main/java/com/hypers/api/Application.java
@@ -2,12 +2,10 @@ package com.hypers.api;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCircuitBreaker
 public class Application {
 
   public static void main(String[] args) {
